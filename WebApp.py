94f1ffd2f020b4b5c993b84b99acba21109b96fe
eddyangang <eddyyang@berkeley.edu>
2020-10-23 12:17:59 -0700

    fixed import error

--- WebApp.py
+++ WebApp.py
@@ -5,7 +5,7 @@
 import pandas as pd 
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import c
+from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 import streamlit as st
 
